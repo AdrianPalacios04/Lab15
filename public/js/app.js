be9036cbec8e96c7fb26efb695b2aa18df9e092b
Adrian Palacios Medrano <adrianpalaciosmedrano@Adrian--MacBook-Air-de-mbtec22.local>
2019-11-29 17:48:11 -0500

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,27 @@
+import { Z_DATA_ERROR } from "zlib";
+
+$(document).ready(function() {
+    var socket = io();
+    $('#formulario').submit(function() {
+        e.preventDefault();
+        var data = {
+            _id:$('#_id').val(),
+            first_name:$('#first_name').val(),
+            last_name:$('#last_name').val(),
+            timezone:$('#timezone').val(),
+            locale:$('#locale').val(),
+            profile_pic:$('#profile_pic').val(),
+        };
+        if(data._id == ''){
+            $('#_id').focus();
+            return alert('Debe ingresar un ID!');
+        }
+        if (data.first_name == '') {
+            $('#first_name').focus();
+            return alert('Debe ingresar un Nombre!')
+        }
+        socket.emit('crear',data)
+        $('#formulario').trigger('reset');
+        return true;
+    }); 
+});
